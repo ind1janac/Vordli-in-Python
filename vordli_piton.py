26f16601304f19c6e29b410d9d0d9645ef3173eb
ind1janac <102441121+ind1janac@users.noreply.github.com>
2022-03-27 17:31:32 +0200

    Update vordli_piton.py

--- vordli_piton.py
+++ vordli_piton.py
@@ -1,7 +1,6 @@
 from colorama import init, Fore, Back, Style
 import random
 init()
-
 loop = True
 word_list = ["strah", "volan", "trava", "lampa", "tepih", "hrast", "tigar", "papir", "covek", "moron", "petak", "metak"]
 while loop:
@@ -14,19 +13,28 @@ while loop:
         word = random.choice(word_list)
         print("Unesi rec, ali pazi rec mora biti 5 slova ili vise!")
 
-        while inner_loop < 6:
+        counter = 29
 
-            attempt = input()
+        while inner_loop < counter:
+
+            attempt = input().lower()
+            if len(attempt) != 5:
+                print("Rec nije validna")
+                continue
 
             # Logika igre
             output = ""
             for i in range(word.__len__()):
                 if attempt[i] == word[i]:
+                    counter-=1
                     output = output + Back.RED + attempt[i] + Back.RESET
                 elif attempt[i] in word:
                     output = output + Back.YELLOW + attempt[i] + Back.RESET
+                    counter-=1
                 else:
                     output = output + attempt[i] + Back.RESET
+                    counter-=1
             print(output)
             if word == attempt:
                 print("Cestitamo domacine, vidim gleda se slagalica")
+                quit()
